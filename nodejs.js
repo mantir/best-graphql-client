f68565bf85a5f3029612d8183f482b2542a86c55
Martin Kapp <mkapp2@gmx.de>
2023-04-06 11:04:11 +0200

    fixing undefined WebSocket in apollo subscriptions

--- nodejs.js
+++ nodejs.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const ws = require('ws');
+global.WebSocket = ws;
 const bestGraphqlClient = require('./bestGraphqlClient');
 
 module.exports = bestGraphqlClient({fetch, ws});
